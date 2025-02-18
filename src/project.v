ea35dbc4bf4ac7a2fd2b48e81969c116ca9b8156
Thejesvinii <146558674+Thejesvinii@users.noreply.github.com>
2025-02-18 17:48:05 +0530

    Update axi.v

--- src/project.v
+++ src/project.v
@@ -1,27 +1,109 @@
-/*
- * Copyright (c) 2024 Your Name
- * SPDX-License-Identifier: Apache-2.0
- */
+timescale 1ns / 1ps
 
-`default_nettype none
-
-module tt_um_example (
-    input  wire [7:0] ui_in,    // Dedicated inputs
-    output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] uio_out,  // IOs: Output path
-    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
-    input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - low to reset
+module axi(
+    input clk,
+    input reset,
+    output reg [7:0] disp_hex_r,
+    output reg [3:0] an,
+   // output reg [7:0] disp_hex_w,
+    // Address read
+    input ms_arvalid, // switch
+    input [3:0] SWM_arADDR,
+    output reg ms_arready,
+    // Read data
+    input ms_rvalid, // switch
+    output reg ms_rready, // LED
+    // Write address
+    input ms_awvalid, // switch
+    //input [3:0] SWM_awADDR,
+    output reg ms_awready, // LED
+    // Write data
+    input ms_wvalid, // switch
+    input [7:0] SWM_wdata,
+    output reg ms_wready, // LED
+    // Write response
+    output reg [1:0] ms_bresp,
+    input sm_bwready
 );
+reg  sm_bwvalid;
+    reg [3:0] ms_araddr, ms_awaddr;
+    reg [7:0] read_data;
+    reg [7:0] memory_w [0:15];
+    reg [7:0] LED_out;
+    // Memory for storage
+    // Initialize memory with all zeros
+        integer i;
+        initial begin
+            for (i = 0; i < 16; i = i + 1)
+                memory_w[i] = i;
+        end
+   
+    always @(posedge clk or posedge reset) begin
+        if (reset) begin
+            ms_araddr <= 4'd0;
+            ms_awaddr <= 4'd0;
+            ms_rready <= 1'b0;
+            ms_awready <= 1'b0;
+            ms_wready <= 1'b0;
+            sm_bwvalid <= 1'b0;
+            ms_bresp <= 2'b00;
+            disp_hex_r <= 8'd0;
+            an <= 4'b0001;
+           // disp_hex_w <= 8'd0;
+        end else begin
+            // Read operation
+            if (ms_arvalid) begin
+                ms_arready<=1'b1;
+                if(ms_arvalid && ms_arready)
+                ms_araddr <= SWM_arADDR;
+                ms_rready <= 1'b1;
+            end
+            if (ms_rvalid && ms_rready) begin
+                disp_hex_r <= LED_out;
+                ms_rready <= 1'b0;
+            end
+     
+            // Write operation
+            if (ms_awvalid) begin  
+                ms_awready <= 1'b1;
+            end
+            if (ms_awready && ms_awvalid) begin
+              ms_awaddr <= SWM_arADDR;
+              ms_awready <= 1'b0;
+            end
+           
+            if (ms_wvalid) begin
+               if(ms_awaddr!=ms_araddr)
+                ms_wready <= 1'b1;
+            end
+           
+            if (ms_wready && ms_wvalid) begin
+                memory_w[ms_awaddr] <= SWM_wdata;
+                //disp_hex_w <= memory_w[ms_awaddr];
+                sm_bwvalid <= 1'b1;
+            end
+            if (sm_bwready && sm_bwvalid) begin
+                ms_bresp <= 2'b11;
+            end
+        end
+    end
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
 
-  // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+always @(*)
+begin
+ case(memory_w[ms_araddr])
+ 8'd0: LED_out = 7'b0000001; // "0"  
+ 8'd1: LED_out = 7'b1001111; // "1"
+ 8'd2: LED_out = 7'b0010010; // "2"
+ 8'd3: LED_out = 7'b0000110; // "3"
+ 8'd4: LED_out = 7'b1001100; // "4"
+ 8'd5: LED_out = 7'b0100100; // "5"
+ 8'd6: LED_out = 7'b0100000; // "6"
+ 8'd7: LED_out = 7'b0001111; // "7"
+ 8'd8: LED_out = 7'b0000000; // "8"  
+ 8'd9: LED_out = 7'b0000100; // "9"
+ default: LED_out = 7'b0000001; // "0"
+ endcase
+end
 
 endmodule
