06c006c3bfb2b423ec98ed6b641a58b99f4de15b
Thejesvinii <146558674+Thejesvinii@users.noreply.github.com>
2025-02-22 08:18:50 -0500

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,40 +1,91 @@
-# SPDX-FileCopyrightText: © 2024 Tiny Tapeout
-# SPDX-License-Identifier: Apache-2.0
-
 import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import ClockCycles
 
-
 @cocotb.test()
-async def test_project(dut):
-    dut._log.info("Start")
-
-    # Set the clock period to 10 us (100 KHz)
+async def test_axi(dut):
+    dut._log.info("start")
     clock = Clock(dut.clk, 10, units="us")
     cocotb.start_soon(clock.start())
 
-    # Reset
-    dut._log.info("Reset")
+    dut._log.info("ena")
     dut.ena.value = 1
-    dut.ui_in.value = 0
-    dut.uio_in.value = 0
+
+    dut._log.info("reset")
     dut.rst_n.value = 0
     await ClockCycles(dut.clk, 10)
+
     dut.rst_n.value = 1
+    await ClockCycles(dut.clk, 2)
 
-    dut._log.info("Test project behavior")
+    # All the bidirectional ports are used for the data_in signal, so they should be inputs
+    assert int(dut.bidirectional_is_output.value) == 0
 
-    # Set the input values you want to test
-    dut.ui_in.value = 20
-    dut.uio_in.value = 30
-
-    # Wait for one clock cycle to see the output values
+    dut._log.info("write 4 bytes to addresses 8, 9, 10, 11")
+    dut.addr.value = 8
+    dut.data_in.value = 0x55
+    dut.we.value = 1
     await ClockCycles(dut.clk, 1)
 
-    # The following assersion is just an example of how to check the output values.
-    # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 50
+    dut.addr.value = 9
+    dut.data_in.value = 0x66
+    dut.we.value = 1
+    await ClockCycles(dut.clk, 1)
 
-    # Keep testing the module by changing the input values, waiting for
-    # one or more clock cycles, and asserting the expected output values.
+    dut.addr.value = 10
+    dut.data_in.value = 0x77
+    dut.we.value = 1
+    await ClockCycles(dut.clk, 1)
+
+    dut.addr.value = 11
+    dut.data_in.value = 0x88
+    dut.we.value = 1
+    await ClockCycles(dut.clk, 1)
+
+    dut._log.info("read back the bytes and verify they are correct")
+    dut.data_in.value = 0
+    dut.addr.value = 8
+    dut.we.value = 0
+    await ClockCycles(dut.clk, 2)
+    assert int(dut.data_out.value) == 0x55
+
+    dut.addr.value = 9
+    await ClockCycles(dut.clk, 2)
+    assert int(dut.data_out.value) == 0x66
+
+    dut.addr.value = 10
+    await ClockCycles(dut.clk, 2)
+    assert int(dut.data_out.value) == 0x77
+
+    dut.addr.value = 11
+    await ClockCycles(dut.clk, 2)
+    assert int(dut.data_out.value) == 0x88
+
+    dut._log.info("write a byte at address 12")
+    dut.addr.value = 12
+    dut.data_in.value = 0x99
+    dut.we.value = 1
+    await ClockCycles(dut.clk, 1)
+
+    dut._log.info("overwrite the byte at address 10")
+    dut.addr.value = 10
+    dut.data_in.value = 0xaa
+    dut.we.value = 1
+    await ClockCycles(dut.clk, 1)
+
+    dut._log.info("read back the bytes and verify they are correct")
+    dut.data_in.value = 0
+    dut.we.value = 0
+    dut.addr.value = 12
+    await ClockCycles(dut.clk, 2)
+    assert int(dut.data_out.value) == 0x99
+
+    dut.addr.value = 10
+    await ClockCycles(dut.clk, 2)
+    assert int(dut.data_out.value) == 0xaa
+
+    dut.addr.value = 8
+    await ClockCycles(dut.clk, 2)
+    assert int(dut.data_out.value) == 0x55
+
+    dut._log.info("all good!")
