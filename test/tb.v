d2ccfa03f17f317c784323396b780342abdf15e4
Thejesvinii <146558674+Thejesvinii@users.noreply.github.com>
2025-02-23 00:37:55 -0500

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -1,49 +1,40 @@
 `default_nettype none
 `timescale 1ns / 1ps
 
-/*
-this testbench just instantiates the module and makes some convenient wires
-that can be driven / tested by the cocotb test.py
-*/
-
 module tb (
-    // testbench is controlled by test.py
     input clk,
     input rst_n,
     input ena,
     input [3:0] SWM_arADDR,
-    input [7:4] SWM_wdata ,
+    input [3:0] SWM_wdata, 
     input ms_arvalid,
     input ms_rready,
     input ms_awvalid,
     input ms_wvalid,
-    input [3:0] bidirectional_is_input,
     output sm_arready,
     output sm_rvalid,
     output sm_awready,
     output sm_wready,
-    output [7:4] bidirectional_is_output,
     output [7:0] disp_hex_r
 );
 
-  // this part dumps the trace to a vcd file that can be viewed with GTKWave
   initial begin
     $dumpfile("tb.vcd");
     $dumpvars(0, tb);
     #1;
   end
 
-  // instantiate the DUT
-  tt_um_thejesvinii_axi (
+  // Instantiate DUT
+  tt_um_thejesvinii_axi uut (
 `ifdef GL_TEST
       .VPWR(1'b1),
       .VGND(1'b0),
 `endif
       .ui_in({SWM_wdata, SWM_arADDR}),
       .uo_out(disp_hex_r),
-      .uio_in({4'b0,ms_wvalid, ms_awvalid,ms_rready,ms_arvalid}),
-      .uio_out({sm_wready,sm_awready,sm_rvalid,sm_arready,4'b0}),
-      .uio_oe({bidirectional_is_output,bidirectional_is_input}),
+      .uio_in({4'b0, ms_wvalid, ms_awvalid, ms_rready, ms_arvalid}),
+      .uio_out({sm_wready, sm_awready, sm_rvalid, sm_arready, 4'b0}),
+      .uio_oe(8'b11110000), // Correct `uio_oe`
       .ena(ena),
       .clk(clk),
       .rst_n(rst_n)
